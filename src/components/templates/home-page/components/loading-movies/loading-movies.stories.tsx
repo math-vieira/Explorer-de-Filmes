06fd7e56488ac33a1bc48ce2fb4c22e619c33c96
math-vieira <matheusvieira9153@gmail.com>
2025-07-27 20:06:15 -0300

    fix: fix loading movies stories

--- src/components/templates/home-page/components/loading-movies/loading-movies.stories.tsx
+++ src/components/templates/home-page/components/loading-movies/loading-movies.stories.tsx
@@ -14,5 +14,9 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {}
+  render: () => (
+    <div className="w-96">
+      <LoadingMovies />
+    </div>
+  )
 };
