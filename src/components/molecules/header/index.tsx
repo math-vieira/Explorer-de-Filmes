0d996d27fb259b176188fdb1764d718bde3191e0
math-vieira <matheusvieira9153@gmail.com>
2025-07-27 16:23:50 -0300

    feat: add elements on page header

--- src/components/molecules/header/index.tsx
+++ src/components/molecules/header/index.tsx
@@ -1,3 +1,31 @@
+import Image from 'next/image';
+import logo from '@/assets/images/logo.png';
+import Link from 'next/link';
+import { FaGithubSquare } from 'react-icons/fa';
+
 export const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="flex w-full items-center justify-between px-5 py-3 shadow-sm">
+      <Link href="/">
+        <Image
+          src={logo.src}
+          alt="logo"
+          width={100}
+          height={100}
+          className="transition-transform duration-200 hover:scale-110"
+        />
+      </Link>
+      <div>
+        <a
+          href="https://github.com/ymath-xyz"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center gap-2 text-black transition-colors duration-200 hover:text-gray-800"
+        >
+          <FaGithubSquare size={24} />
+          <span className="font-medium">Matheus Vieira</span>
+        </a>
+      </div>
+    </header>
+  );
 };
